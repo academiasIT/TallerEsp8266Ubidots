cd9b659eaca9fe2d83dd220346ebabb6eaa0230f
rodrigo <rmunozl@inacap.cl>
2018-05-08 10:33:08 -0300

    se agrega script de arduino y readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# TallerEsp8266Ubidots
+# Taller ArduinoEsp8266 y Ubidots
+
+## instrucciones
+
+### creacion cuenta en Ubidots
+1. ingrese a la página de uBidots [app.ubidots.com](https://app.ubidots.com) y registrese (pestaña SING UP)
+2. En la pagina de uBidots, seleccione la pestaña "Device" y luego haga click en "Add Device" como se muestra en la siguiente figura ![ubidots-create-device](images/2018/05/create-device.gif)
+Luego asigne un nombre representativo al nuevo dispositivo. Esto creará una representación de nuestra trajetaa Esp8266 en nuestra cuenta de uBidots
+
+3. A continuación haga click en el dispositivo recien creado y agregue una variable mediante un click "Add Variable", seleccione la opción "Default" y nombre esta variable como "Temperatura".
+4. Cree otra variable llamada "Humedad"
+5. Por último, cree una tercera variable llamada "Led"
---
+++ arduino_ubidots/arduino_ubidots.ino
@@ -0,0 +1,64 @@
+#include <Arduino.h>
+
+#include "UbidotsMicroESP8266.h"
+#include "DHT.h"
+
+#define TOKEN  ""  // Ubidots TOKEN
+#define ID_T "" // Temperatura ID 
+#define ID_H "" // Humedad ID
+#define ID_SL ""// led ID 
+
+#define WIFISSID "ssid" //Wi-Fi SSID
+#define PASSWORD "passwd" //Wi-Fi password
+
+#define DHTPIN 2 //pin en donde se conectara el pin de datos del sensor DHT11
+#define DHTYPE DHT11 //tipo de sensor DHT11
+
+#define LAMP 5//pin en donde se conectara el led
+
+DHT dht(DHTPIN, DHTYPE);
+Ubidots client(TOKEN);
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LAMP,OUTPUT);
+  pinMode(DHTPIN,INPUT);
+  digitalWrite(LAMP,HIGH);
+  
+  Serial.begin(115200);
+  client.wifiConnection(WIFISSID, PASSWORD);
+  
+  dht.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+
+  if(isnan(t) || isnan(h)){
+    Serial.println("Error al leer el sensor DHT 11");
+  }else{
+    Serial.print("Temp: ");
+    Serial.println(t);
+    Serial.print("Humedad: ");
+    Serial.println(h);
+
+    client.add(ID_T, t);
+    client.add(ID_H, h);
+
+    client.sendAll(false);
+  }
+  
+  float swLamp = client.getValue(ID_SL);
+  Serial.print(swLamp);
+  if(swLamp==1.0){
+    Serial.println("Led on");
+    digitalWrite(LAMP,LOW);
+  } else {
+    Serial.println("Led off");
+    digitalWrite(LAMP,HIGH);
+  }
+  
+  delay(3000);
+}
